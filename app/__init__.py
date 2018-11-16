e974d324814a3ac44d335a38f4b6c1e9f90bf3b7
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-16 06:06:51 +0300

    [bg #161992198] to fix typo on message returned

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,12 @@ from .api.v1 import v1
 def create_app(config_name):
             app = Flask(__name__, instance_relative_config=True)
             app.config.from_object(app_config[config_name])
-
             app.register_blueprint(v1)
 
             @app.errorhandler(404)
             def url_doesnt_exist(error):
                 return jsonify({
-                    "Message": "Check your url snd ensure it is the correct one"
+                    "Message": "Check your url and ensure it is the correct one as per documentation"
                 }), 404
             app.url_map.strict_slashes = False
             return app
