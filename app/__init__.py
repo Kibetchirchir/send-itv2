0645755e6a8491b16b1e78a386669a24f947f82b
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-16 05:40:53 +0300

    [start #161992198]protect URL to give a message when error 404 is thrown

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from instance.config import app_config
 from .api.v1 import v1
 
@@ -8,5 +8,11 @@ def create_app(config_name):
             app.config.from_object(app_config[config_name])
 
             app.register_blueprint(v1)
+
+            @app.errorhandler(404)
+            def url_doesnt_exist(error):
+                return jsonify({
+                    "Message": "Check your url snd ensure it is the correct one"
+                }), 404
             app.url_map.strict_slashes = False
             return app
