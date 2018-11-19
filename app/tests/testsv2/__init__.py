0a01b668599e1a150e5b4c717eda388ed333d177
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-20 00:48:57 +0300

    [ch #161993537]add test skeleton for our version2

---
+++ app/tests/testsv2/__init__.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+from app import create_app
+from ...db_config import destroy_tables
+
+
+class BaseClass(unittest.TestCase):
+    """This base class for our testcases"""
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.user = {"name": "chirchir Kibet",
+                     "email": "langatchirchir@gmail.com",
+                     "role": "user",
+                     "password": "kevin12345"}
+        self.admin = {"name": "admin",
+                      "email": "admin@gmail.com",
+                      "role": "admin",
+                      "password": "admin"}
+        self.parcel = {"user_id": 1,
+                       "parcel_type": "letter",
+                       "recepient_number": "254715428709",
+                       "Dest": "Moi_avenue",
+                       "status": "on_transit"}
+     def tearDown(self):
+         destroy_tables()
+         
