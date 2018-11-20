ea416171ed898f5cf188e248e63c1b4abc45ee13
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-20 16:10:44 +0300

    [ft 162038593]to initiate database when creating the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,12 @@ from .api.v1 import v1
 from .api.v2 import v2
 from .db_config import create_tables
 
-create_tables()
-
 
 def create_app(config_name):
     """This method creates the aplication and registers the app"""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
+    create_tables()
     app.register_blueprint(v1)
     app.register_blueprint(v2)
 
