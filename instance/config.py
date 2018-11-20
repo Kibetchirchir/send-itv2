3ecac2e5482806f989b11a56e6b8976dca1d171e
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-20 09:51:34 +0300

    [ch #161993537] add develpoment configs

--- instance/config.py
+++ instance/config.py
@@ -13,6 +13,7 @@ class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
     TESTING = True
+    DB_URL = os.getenv("DB_TEST")
 
 
 class TestingConfig(Config):
