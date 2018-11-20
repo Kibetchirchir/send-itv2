cfbf02a97cbe0c09be2e995e5f042db33e653c3c
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-20 16:14:22 +0300

    [ft #162038593] check for email if user exists

--- app/api/v2/views/user.py
+++ app/api/v2/views/user.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify, make_response
 from flask_restplus import Api, Resource
 from ..models.user import UserModel
+from .validator import CheckRequired
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,8 +13,21 @@ class Users(Resource):
     def post(self):
         """The post request for the class"""
         payload = api.payload
-        if not payload:
+        # check if payload is empty
+        check = CheckRequired(payload)
+        checked_payload = check.check_payload_signup()
+        if not checked_payload:
             return {'status': 'failed', 'message': 'bad request refer to API document or provide all fields'}, 400
+        verifier = CheckRequired(payload)
+        email_checked = verifier.check_for_email()
+        if not email_checked:
+            return {'status': 'failed', 'message': 'bad request check your email'}, 400
+        email = payload['email']
+        user = UserModel()
+        data = user.get_user(email)
+        print(data)
+        if data:
+            return {'status': 'failed', 'message': 'conflict email already used'}, 409
         user = UserModel()
         data = user.add_user(payload)
         return {'status': 'added', 'message': 'Successfully signed up', 'data': data}, 201
