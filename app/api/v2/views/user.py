f21fe3acfc556b57efa601e35020e6499e1e6bcc
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-21 06:02:15 +0300

    [ft #162038619]create view for our login

--- app/api/v2/views/user.py
+++ app/api/v2/views/user.py
@@ -30,3 +30,39 @@ class Users(Resource):
         user = UserModel()
         data = user.add_user(payload)
         return {'status': 'added', 'message': 'Successfully signed up', 'data': data}, 201
+
+
+class Login(Resource):
+    """The login class that will handle login"""
+    def post(self):
+        """The post request for login"""
+        payload = api.payload
+        # Get the variables you'll use
+        if not payload:
+            return {'status': 'failed', 'message': 'provide all fields'}, 400
+        if not all(key in payload for key in ['email', 'password', 'role']):
+            return {'status': 'failed', 'message': 'bad request please provide all fields'}, 400
+        email = payload['email']
+        role_provided = payload['role']
+        password_given = payload['password']
+        user = UserModel()
+        data = user.get_user(email)
+        if not data:  # returned from our model get_user
+            return {'status': 'failed', 'message': 'email not found'}, 401
+        # True for admin and False for user
+        role = data[4]
+        if role:
+            role = "admin"
+        else:
+            role = "user"
+        password = data[2]
+        if not password == password_given:
+            return {'status': 'failed', 'message': 'wrong password'}, 401
+        if role_provided == 'admin' and role == 'user':
+            return {'status': 'failed', 'message': 'you are not an admin'}, 403
+        payload = {"name": data[3],
+                   "role": role,
+                   "user_id": data[0]
+                   }
+        return {'status': 'success', 'message': 'Successful logged in', 'data': payload}, 200
+
