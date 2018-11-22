613dd5432ad84c189368b3f455d2560bcd736ff8
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-22 21:45:52 +0300

    [ft #162038834]add database for our change status

--- app/api/v2/models/parcel.py
+++ app/api/v2/models/parcel.py
@@ -97,3 +97,13 @@ class ParcelModel:
                           }
                 results.append(parcel)
         return results
+
+    def change_status(self, status, order_id):
+        query = """update parcels set status ='{}' where parcel_id='{}';""".format(status, order_id)
+        cur = self.con.cursor()
+        cur.execute(query)
+        print(query)
+        count = cur.rowcount
+        print(count)
+        if count > 0:
+            return True
