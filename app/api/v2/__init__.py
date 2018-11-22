982b91f012f8bb0d396664803e51b399e6a717c7
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-23 01:47:16 +0300

    [ft #162038898]to add test

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from flask_restful import Api
 from .views.user import Users
 from .views.user import Login
-from .views.parcel import Parcels, ParcelChangeDestination, AdminChangeStatus
+from .views.parcel import Parcels, ParcelChangeDestination, AdminChangeStatus, AdminChangeLocation
 
 
 v2 = Blueprint('apiv2', __name__, url_prefix=("/api/v2"))
@@ -14,6 +14,7 @@ api.add_resource(Login, '/auth/login', strict_slashes=False)
 api.add_resource(Parcels, '/parcels', strict_slashes=False)
 api.add_resource(ParcelChangeDestination, '/parcels/<parcelid>/destination', strict_slashes=False)
 api.add_resource(AdminChangeStatus, '/parcels/<parcelid>/status', strict_slashes=False)
+api.add_resource(AdminChangeLocation, '/parcels/<parcelid>/presentLocation', strict_slashes=False)
 
 
 
