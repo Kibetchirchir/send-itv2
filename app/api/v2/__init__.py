c0f768b0f8ba185dd98fb91290f5de0cff8e7287
Kibet Chirchir <langatchirchir@gmail.com>
2018-11-22 04:17:14 +0300

    [ft #162038740]add the resource for adding the parcels

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask_restful import Api
 from .views.user import Users
 from .views.user import Login
+from .views.parcel import Parcels
 
 
 v2 = Blueprint('apiv2', __name__, url_prefix=("/api/v2"))
@@ -10,5 +11,6 @@ api = Api(v2)
 
 api.add_resource(Users, '/auth/signup', strict_slashes=False)
 api.add_resource(Login, '/auth/login', strict_slashes=False)
+api.add_resource(Parcels, '/parcels', strict_slashes=False)
 
 
