c729c83fc6bb2ba67c0ca2356df2281f13a6bddc
Kelvin Chirchir <langatchirchir@gmail.com>
2019-02-13 18:58:07 +0300

    [finish #162038947] to remove email when testing

--- app/api/v2/views/mailing.py
+++ app/api/v2/views/mailing.py
@@ -8,23 +8,26 @@ class SendMail:
 
     def send_mail(self, message, to, subject):
         # Gmail Sign In
-        gmail_sender = os.getenv("EMAIL")
-        gmail_passwd = os.getenv("EMAIL_PASS")
+        if not os.getenv('FLASK_ENV') == 'testing':
+            gmail_sender = os.getenv("EMAIL")
+            gmail_passwd = os.getenv("EMAIL_PASS")
 
-        try:
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.ehlo()
-            server.starttls()
-            server.login(gmail_sender, gmail_passwd)
+            try:
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.ehlo()
+                server.starttls()
+                server.login(gmail_sender, gmail_passwd)
 
-            BODY = '\r\n'.join(['To: %s' % to,
-                                'From: %s' % gmail_sender,
-                                'Subject: %s' % subject,
-                                '', message])
+                BODY = '\r\n'.join(['To: %s' % to,
+                                    'From: %s' % gmail_sender,
+                                    'Subject: %s' % subject,
+                                    '', message])
 
-            server.sendmail(gmail_sender, [to], BODY)
-            res = "sent"
-        except:
-            res = "fail"
-        server.quit()
-        return res
+                server.sendmail(gmail_sender, [to], BODY)
+                res = "sent"
+            except:
+                res = "fail"
+            server.quit()
+            return res
+        return True
+
